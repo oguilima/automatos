b4f60cf6043f5ed29f4c729d8f9b277744c2a204
oguilima_mfx <guilherme.lima@grupomfx.com>
2022-10-03 21:00:30 -0300

    ajuste links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function abreItem(projeto){
     console.log(projeto)
 
-    projeto == 'vending' ? window.location.href = './vendingmachine/' : window.location.href = '../elevador/'
+    projeto == 'vending' ? window.location.href = './vendingmachine/index.html' : window.location.href = './elevador/index.html'
 
     /*if(projeto == 'vending'){
         window.location.href = './vendingmachine/'
